4c86f02cade9dfc3808adb6ce85a0cd31b1ee1ab
mstevenacero <63487908+mstevenacero@users.noreply.github.com>
2021-08-22 11:57:03 -0500

    primera pantalla de carga

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
+  {path:'',redirectTo:'login',pathMatch:'full'},
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
