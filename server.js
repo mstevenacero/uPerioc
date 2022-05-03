693c2fbd3c9923b193b35809ce5b128900325fd5
Michael Acero <michaelacero@MacBook-Air-de-Michael.local>
2022-05-03 11:57:16 -0500

    cambios

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/www'));
+app.use(express.static('./www'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'www/'}),
